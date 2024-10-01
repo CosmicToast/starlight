04d94f4e5c814cfb33fcb24dcd166b92ac174b47
Pavel Löbl <pavel@loebl.cz>
2024-10-02 00:16:09 +0200

    alacritty: switching to TOML configuration format
    Alacritty is using TOML instead of YAML now.

--- colors/alacritty.sh
+++ colors/alacritty.sh
@@ -3,31 +3,30 @@
 
 cat <<HEREDOC
 # $theme
-colors:
-  # Default colors
-  primary:
-    foreground: '#$foreground'
-    background: '#$background'
+# Default colors
+[colors.primary]
+foreground = '#$foreground'
+background = '#$background'
 
-  # Normal colors
-  normal:
-    black:   '#$black'
-    red:     '#$red'
-    green:   '#$green'
-    yellow:  '#$yellow'
-    blue:    '#$blue'
-    magenta: '#$magenta'
-    cyan:    '#$cyan'
-    white:   '#$white'
+# Normal colors
+[colors.normal]
+black   = '#$black'
+red     = '#$red'
+green   = '#$green'
+yellow  = '#$yellow'
+blue    = '#$blue'
+magenta = '#$magenta'
+cyan    = '#$cyan'
+white   = '#$white'
 
-  # Bright colors
-  bright:
-    black:   '#$brblack'
-    red:     '#$brred'
-    green:   '#$brgreen'
-    yellow:  '#$bryellow'
-    blue:    '#$brblue'
-    magenta: '#$brmagenta'
-    cyan:    '#$brcyan'
-    white:   '#$brwhite'
+# Bright colors
+[colors.bright]
+black   = '#$brblack'
+red     = '#$brred'
+green   = '#$brgreen'
+yellow  = '#$bryellow'
+blue    = '#$brblue'
+magenta = '#$brmagenta'
+cyan    = '#$brcyan'
+white   = '#$brwhite'
 HEREDOC
